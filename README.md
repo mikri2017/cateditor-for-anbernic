ecf5a9ad3a8f4cd5417652ddf91b0a35c202a08a
MIKRI <2007mik007@mail.ru>
2025-07-19 19:40:59 +0700

    Описал проект, добавил скрипт, формирующий основное окно программы

---
+++ README.md
@@ -0,0 +1,15 @@
+# CatEditor For Anbernic:
+
+Редактор каталога для папок каталогов с играми консоли Anbernic.
+
+Тестировал на своей Anbernic RG ARC-S.
+
+## Зависимости
+
+* Модули python:
+  * wxpython
+  * pyinstaller (для сборки в портативный формат)
+
+## Сборка
+
+pyinstaller ./cat_editor.py --onefile
---
+++ cat_editor.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import wx
+import time
+
+class MainFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, title="CatEditor For Anbernic")
+
+        self.SetMaxSize((800, 755))
+        self.SetMinSize((800, 755))
+
+
+if __name__ == "__main__":
+    # Инициализация приложения
+
+    # Делаем папку расположения exe'шника скрипта рабочей
+    work_dir = os.path.dirname(sys.executable)
+    os.chdir(work_dir)
+
+    log_folder_path = "logs"
+
+    if not os.path.exists(log_folder_path):
+        try:
+            os.makedirs(log_folder_path)
+        except Exception as ex:
+            error_msg = "Не удалось создать папку %s\r\n\r\n%s" % (
+                log_folder_path,
+                str(ex)
+            )
+
+            print(error_msg)
+            time.sleep(10)
+            sys.exit(1)
+
+    print("Скрипт запустил работу")
+
+    info_msg = "Рабочая папка: %s" % work_dir
+    print(info_msg)
+
+    app = wx.App()
+    MainFrame().Show()
+    app.MainLoop()
+
+    print("Скрипт завершил работу")
