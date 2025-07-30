bb42ef24334a9c4d82b76c41766b055e41879761
MIKRI <2007mik007@mail.ru>
2025-07-30 20:18:36 +0700

    Раскидал функции интерфейса редактора каталога по отдельным классам-панелям, добавил очистку при смене папки, если в ней файлов нет

--- pnl_cat_games_editor.py
+++ pnl_cat_games_editor.py
@@ -1,12 +1,12 @@
 import wx
-from cat_info_mgr import CatInfoMgr
+from cat_gamelist_mgr import CatGamelistMgr
 
-class PnlCatInfo(wx.Panel):
+class PnlCatGamesEditor(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
 
-        # Общая информация
-        self.__info_mgr = CatInfoMgr()
+        # Каталог игр
+        self.__gamelist_mgr = CatGamelistMgr()
 
         # Текст последней ошибки
         self.__err_msg = ""
@@ -16,9 +16,22 @@ class PnlCatInfo(wx.Panel):
         self.__sizer_main = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.__sizer_main)
 
-        self.__txt_ctrl = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.__sizer_main.Add(self.__txt_ctrl, 1, wx.TOP|wx.EXPAND, 10)
-        self.__txt_ctrl.Bind(wx.EVT_TEXT, self.txt_ctrl_update_info)
+        self.__stb_game_list = wx.StaticBox(self, wx.ID_ANY, "Список игр")
+        self.__sizer_stb_game_list = wx.StaticBoxSizer(self.__stb_game_list, orient=wx.VERTICAL)
+        self.__sizer_main.Add(self.__sizer_stb_game_list, 1, wx.TOP|wx.EXPAND, 0)
+
+        self.__lb_games = wx.ListBox(self.__stb_game_list)
+        self.__sizer_stb_game_list.Add(self.__lb_games, 1, wx.EXPAND)
+
+        self.__stb_g_add_del = wx.StaticBox(self.__stb_game_list, wx.ID_ANY)
+        self.__sizer_g_add_del = wx.StaticBoxSizer(self.__stb_g_add_del, orient=wx.HORIZONTAL)
+        self.__sizer_stb_game_list.Add(self.__sizer_g_add_del, 0, wx.BOTTOM|wx.EXPAND)
+
+        self.__sizer_g_add_del.AddStretchSpacer(prop=1)
+        self.__btn_g_add = wx.Button(self.__stb_g_add_del, wx.ID_ANY, "Добавить")
+        self.__sizer_g_add_del.Add(self.__btn_g_add, 0, wx.RIGHT)
+        self.__btn_g_del = wx.Button(self.__stb_g_add_del, wx.ID_ANY, "Удалить")
+        self.__sizer_g_add_del.Add(self.__btn_g_del, 0, wx.RIGHT)
 
         self.__stb_ctrls = wx.StaticBox(self, wx.ID_ANY)
         self.__sizer_stb_ctrls = wx.StaticBoxSizer(self.__stb_ctrls, orient=wx.HORIZONTAL)
@@ -43,30 +56,22 @@ class PnlCatInfo(wx.Panel):
         return self.__err_msg
 
 
-    def get_file_path(self):
-        """Получить путь к файлу с информацией
+    def set_xml_file_path(self, filepath, reset_data = False):
+        """Задать путь к XML файлу каталога игр
 
-        :return: Путь к файлу
-        """
-
-        return self.__info_mgr.get_file_path()
-
-
-    def set_file_path(self, filepath, reset_data = False):
-        """Задать путь к файлу с информацией
-
-        :param filepath: Путь к текстовому файлу с информацией
+        :param filepath: Путь к XML файлу каталога игр
+        :param reset_data: Сброс текущих данных
         :return: Функция не возвращает результат
         """
 
         if reset_data is True:
             # Удалим данные по играм
-            self.__info_mgr = CatInfoMgr()
+            self.__gamelist_mgr = CatGamelistMgr()
 
             # Очистим интерфейс
-            self.__txt_ctrl.Clear()
+            self.__lb_games.Clear()
 
-        self.__info_mgr.set_file_path(filepath)
+        self.__gamelist_mgr.set_xml_file_path(filepath)
 
 
     def reload_from_file(self):
@@ -75,13 +80,16 @@ class PnlCatInfo(wx.Panel):
         :return: True, в случае успеха
         """
 
-        self.__txt_ctrl.Clear()
+        # Наполняем список игр
+        self.__lb_games.Clear()
 
-        if self.__info_mgr.load_info_from_file() is False:
-            self.__err_msg = self.__info_mgr.get_last_error()
+        if self.__gamelist_mgr.parse_xml_file() is False:
+            self.__err_msg = self.__gamelist_mgr.get_last_error()
             return False
 
-        self.__txt_ctrl.WriteText(self.__info_mgr.get_info())
+        for game_name in self.__gamelist_mgr.get_game_names():
+            self.__lb_games.Append(game_name)
+
         return True
 
 
@@ -102,20 +110,8 @@ class PnlCatInfo(wx.Panel):
         :return: Функция не возвращает результат
         """
 
-        if self.__info_mgr.save_info_to_file() is False:
-            self.__err_msg = self.__info_mgr.get_last_error()
+        if self.__gamelist_mgr.save_to_xml_file() is False:
+            self.__err_msg = self.__gamelist_mgr.get_last_error()
             return False
 
         return True
-
-
-    def txt_ctrl_update_info(self, event):
-        """Изменение общей информации при её редактировании
-        в интерфейсе приложения
-
-        :param event: Событие элемента графического интерфейса
-        :return: Функция не возвращает результат
-        """
-
-        txt_ctrl = event.GetEventObject()
-        self.__info_mgr.set_info(txt_ctrl.GetValue())
--- pnl_main_frm.py
+++ pnl_main_frm.py
@@ -1,7 +1,7 @@
 import wx
 import os
-from cat_info_mgr import CatInfoMgr
-from cat_gamelist_mgr import CatGamelistMgr
+from pnl_cat_info import PnlCatInfo
+from pnl_cat_games_editor import PnlCatGamesEditor
 
 class PnlMainFrm(wx.Panel):
     def __init__(self, parent):
@@ -19,12 +19,6 @@ class PnlMainFrm(wx.Panel):
             }
         }
 
-        # Общая информация
-        self.__info_mgr = CatInfoMgr()
-
-        # Каталог игр
-        self.__gamelist_mgr = CatGamelistMgr()
-
         # Рисуем интерфейс приложения
         # Основной sizer под 3 блока интерфейса
         self.__sizer_main = wx.BoxSizer(wx.VERTICAL)
@@ -36,9 +30,15 @@ class PnlMainFrm(wx.Panel):
         self.__sizer_main.Add(self.__sizer_stb_path, 0, wx.TOP|wx.EXPAND)
 
         # Блок 2. Редактор каталога игр
-        self.__stb_games_cat = wx.StaticBox(self, wx.ID_ANY, "Редактор каталога игр")
-        self.__sizer_stb_games_cat = wx.StaticBoxSizer(self.__stb_games_cat, orient=wx.VERTICAL)
-        self.__sizer_main.Add(self.__sizer_stb_games_cat, 1, wx.TOP|wx.EXPAND, 0)
+        self.__nb_games_cat = wx.Notebook(self)
+        self.__sizer_main.Add(self.__nb_games_cat, 1, wx.EXPAND)
+
+        # Каталог игр
+        self.__tab_cat_game_ed = PnlCatGamesEditor(self.__nb_games_cat)
+        self.__nb_games_cat.AddPage(self.__tab_cat_game_ed, "Редактор каталога игр")
+
+        self.__tab_cat_inf = PnlCatInfo(self.__nb_games_cat)
+        self.__nb_games_cat.AddPage(self.__tab_cat_inf, "Общая информация")
 
         # Блок 3. Сообщения о проблемах
         self.__stb_warn_msgs = wx.StaticBox(self, wx.ID_ANY, "Сообщения об ошибках")
@@ -69,36 +69,6 @@ class PnlMainFrm(wx.Panel):
         self.__sizer_stb_path.Add(self.__btn_cat_folder_set, 0, wx.RIGHT|wx.EXPAND, border=5)
         self.__btn_cat_folder_set.Bind(wx.EVT_BUTTON, self.btn_cat_folder_set_click)
 
-        # Заполняем элементами Блок 2
-        self.__pnl_gc_editor = wx.Panel(self.__stb_games_cat)
-        self.__bsizer_gc_editor = wx.BoxSizer(wx.HORIZONTAL)
-        self.__pnl_gc_editor.SetSizer(self.__bsizer_gc_editor)
-        self.__sizer_stb_games_cat.Add(self.__pnl_gc_editor, 1, wx.TOP|wx.EXPAND)
-
-        self.__pnl_gc_ctrl = wx.Panel(self.__stb_games_cat)
-        self.__bsizer_gc_ctrl = wx.BoxSizer(wx.HORIZONTAL)
-        self.__pnl_gc_ctrl.SetSizer(self.__bsizer_gc_ctrl)
-        self.__sizer_stb_games_cat.Add(self.__pnl_gc_ctrl, 0, wx.BOTTOM|wx.EXPAND)
-
-        # Кнопки управления редактором
-        self.__bsizer_gc_ctrl.AddStretchSpacer(prop=1)
-
-        self.__btn_gc_reset = wx.Button(self.__pnl_gc_ctrl, wx.ID_ANY, "Сброс")
-        self.__bsizer_gc_ctrl.Add(self.__btn_gc_reset, 0, wx.RIGHT, border=5)
-        self.__btn_gc_reset.Bind(wx.EVT_BUTTON, self.btn_gc_reset_click)
-
-        self.__btn_gc_save = wx.Button(self.__pnl_gc_ctrl, wx.ID_ANY, "Сохранить")
-        self.__bsizer_gc_ctrl.Add(self.__btn_gc_save, 0, wx.RIGHT, border=5)
-        self.__btn_gc_save.Bind(wx.EVT_BUTTON, self.btn_gc_save_click)
-
-        # Список игр
-        self.__stb_game_list = wx.StaticBox(self.__pnl_gc_editor, wx.ID_ANY, "Игры")
-        self.__sizer_stb_game_list = wx.StaticBoxSizer(self.__stb_game_list, orient=wx.HORIZONTAL)
-        self.__bsizer_gc_editor.Add(self.__sizer_stb_game_list, 1, wx.LEFT|wx.EXPAND)
-
-        self.__lb_games = wx.ListBox(self.__stb_game_list)
-        self.__sizer_stb_game_list.Add(self.__lb_games, 0, wx.EXPAND)
-
 
     def get_open_folder_path(self):
         res = {
@@ -125,10 +95,6 @@ class PnlMainFrm(wx.Panel):
         :return: Функция не возвращает результат
         """
 
-        # Сброс загруженных данных
-        self.__info_mgr = CatInfoMgr()
-        self.__gamelist_mgr = CatGamelistMgr()
-
         folder_path = self.__txtctrl_cat_folder.GetValue()
         if folder_path == "":
             header = f"Редактор каталога игр Anbernic"
@@ -146,25 +112,17 @@ class PnlMainFrm(wx.Panel):
         info_file = self.__d_files['info']['name']
         info_path = os.path.join(folder_path, info_file)
 
-        self.__info_mgr.set_file_path(info_path)
+        self.__tab_cat_inf.set_file_path(info_path, True)
         if self.__d_files['info']['exist'] is True:
-            self.__info_mgr.load_info_from_file()
+            self.__tab_cat_inf.reload_from_file()
 
         # Каталог игр
         gamelist_file = self.__d_files['gamelist']['name']
         gamelist_path = os.path.join(folder_path, gamelist_file)
 
-        self.__gamelist_mgr.set_xml_file_path(gamelist_path)
+        self.__tab_cat_game_ed.set_xml_file_path(gamelist_path, True)
         if self.__d_files['gamelist']['exist'] is True:
-            self.__gamelist_mgr.parse_xml_file()
-
-        # Наполняем список игр
-        self.__lb_games.Clear()
-        for game_name in self.__gamelist_mgr.get_game_names():
-            self.__lb_games.Append(game_name)
-
-        print("Информация:")
-        print(self.__info_mgr.get_info())
+            self.__tab_cat_game_ed.reload_from_file()
 
 
     def btn_cat_folder_set_click(self, event):
@@ -178,34 +136,3 @@ class PnlMainFrm(wx.Panel):
         if folder_path_res['res']:
             self.__txtctrl_cat_folder.SetValue(folder_path_res['folder_path'])
             self.__reload_from_files()
-
-
-    def btn_gc_reset_click(self, event):
-        """Сбросить изменения каталога игр (перечитать с файлов)
-
-        :param event:
-        :return: Функция не возвращает результат
-        """
-
-        self.__reload_from_files()
-
-
-    def btn_gc_save_click(self, event):
-        """Сохранить сделанные изменения в файлы
-
-        :param event:
-        :return: Функция не возвращает результат
-        """
-
-        folder_path = self.__txtctrl_cat_folder.GetValue()
-        if folder_path == "":
-            header = f"Редактор каталога игр Anbernic"
-            msg = "Не выбран путь к папке каталога игр!"
-            wx.MessageBox(msg, header, wx.OK | wx.ICON_ERROR)
-            return
-
-        # Сохраняем информацию о каталоге
-        pass
-
-        # Сохраняем каталог игр
-        self.__gamelist_mgr.save_to_xml_file()
